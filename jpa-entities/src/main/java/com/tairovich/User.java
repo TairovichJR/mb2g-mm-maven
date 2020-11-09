a15cbcf3afa5ad7343c04c32ee8dd33e37b43af1
TairovichJR <yokubov.omonjon@gmail.com>
2020-11-09 18:30:00 -0500

    entities module

---
+++ jpa-entities/src/main/java/com/tairovich/User.java
@@ -0,0 +1,23 @@
+package com.tairovich;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
