c0c9929a7a745c9e3ef44ba40a4dbaf531e0f047
TairovichJR <yokubov.omonjon@gmail.com>
2020-11-09 20:13:27 -0500

    web app branch

---
+++ web-app/src/main/java/com/tairovich/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.tairovich.controllers;
+
+import com.tairovich.User;
+import com.tairovich.domain.UserCommand;
+
+public class UserController {
+
+    public User saveUser(UserCommand command){
+        //fake impl
+        return new User();
+    }
+
+}
